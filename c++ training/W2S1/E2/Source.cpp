327ac96fafb505fec0889057d16e1a6cc6c153d3
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-14 10:37:32 -0500

    Exercise 2
    Working Exercise 2 upload

---
+++ c++ training/W2S1/E2/Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+//Exercise 2 - Military Clock
+//Create a class called MilitaryClock that stores and displays the military time.
+//
+//Your class should contain the following constructors and methods :
+//	MilitaryClock();
+//MilitaryClock(short hours, short minutes);
+//void SetTime(short hours, short minutes);
+//void DisplayTime();
+//
+//Here’s an example of how the class could be used :
+//MilitaryClock clock;
+//clock.Display();
+//clock = MilitaryClock(7, 0);
+//clock.Display();
+//clock.SetTime(12, 15);
+//clock.Display();
+//
+//Don’t forget that military time is displayed as hhmm so add a preceding 0 before hours / minutes if their value is less than 10.
+
+class MilitaryClock {
+private:
+
+	short m_hours, m_minutes;
+
+public:
+
+	MilitaryClock() {
+		m_hours = 0;
+		m_minutes = 0;
+	}
+
+	MilitaryClock(short hours, short minutes) {
+		m_hours = hours;
+		m_minutes = minutes;
+	}
+
+	void SetTime(short hours, short minutes) {
+		m_hours = hours;
+		m_minutes = minutes;
+	}
+
+	void Display() {
+		std::cout << addOptionalZero(m_hours) << m_hours << addOptionalZero(m_minutes) << m_minutes << std::endl;
+	}
+
+	std::string addOptionalZero(short input) {
+		std::string formattedString = "";
+		if (input < 10) formattedString += '0';
+		return formattedString;
+	}
+};
+
+int main() {
+	MilitaryClock clock;
+	clock.Display();
+	clock = MilitaryClock(7, 0);
+	clock.Display();
+	clock.SetTime(12, 15);
+	clock.Display();
+}
