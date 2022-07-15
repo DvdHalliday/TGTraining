32f41665d5a71b96db9dd9f52cdf2021d8733e7f
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-15 16:15:21 -0500

    Exercise 2
    Uploading the Playlist class files

---
+++ c++ training/W2S2/Spotify/Playlist.cpp
@@ -0,0 +1,45 @@
+#include "Playlist.h"
+
+Playlist::Playlist()
+{
+	setPlaylistTitle("THIS IS THE BEST PLAYLIST EVER");
+	m_songCount = 0;
+}
+
+Playlist::Playlist(std::string playlistTitle)
+{
+	setPlaylistTitle(playlistTitle);
+	m_songCount = 0;
+}
+
+void Playlist::setPlaylistTitle(std::string playlistTitle)
+{
+	m_playlistTitle = playlistTitle;
+}
+
+std::string Playlist::getPlaylistName() const
+{
+	return m_playlistTitle;
+}
+
+bool Playlist::addSong(Song song)
+{
+	if (m_songCount == m_MaxSongCount || !song.isValid()) return false;
+	m_songs[m_songCount] = song;
+	m_songCount++;
+	return true;
+}
+
+Song Playlist::getSong(int songIndex) const
+{
+	return m_songs[songIndex];
+}
+
+void Playlist::displayPlaylist() const
+{
+	std::cout << "Song Title\t\tArtist Name\t\tDuration" << std::endl;
+	std::cout << "-----------------------------------------------------------------------" << std::endl;
+	for (int i = 0; i < m_songCount; i++) {
+		std::cout << m_songs[i].getSongTitle() << "\t\t" << m_songs[i].getArtistName() << "\t\t" << m_songs[i].getDuration() << std::endl;
+	}
+}
---
+++ c++ training/W2S2/Spotify/Playlist.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include "Song.h"
+
+//Create a new class called Playlist.Playlists should be containers for songs, so it should let the user add songs to the playlist.
+//
+//It should have the following private members.
+//const static int MaxSongCount;
+//std::string playlistTitle;
+//Song songs[MaxSongCount];
+//int songCount;
+//
+//The class should have a default constructor, a constructor that initializes the playlist title, and a getterand setter for the playlist name.
+//
+//Additionally, implement the following methods :
+//bool addSong(Song song);
+//Song getSong(int songIndex);
+//void displayPlaylist();
+
+
+class Playlist {
+private:
+	const static int m_MaxSongCount = 25;
+	std::string m_playlistTitle;
+	Song m_songs[m_MaxSongCount];
+	int m_songCount;
+public:
+	Playlist();
+	Playlist(std::string playlistTitle);
+	void setPlaylistTitle(std::string playlistTitle);
+	std::string getPlaylistName() const;
+	bool addSong(Song song);
+	Song getSong(int songIndex) const;
+	void displayPlaylist() const;
+};
