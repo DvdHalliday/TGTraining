f3d3485a2e6f12472b389ebcd16148e1701fac6e
David <109089386+DvdHalliday@users.noreply.github.com>
2022-08-24 16:19:07 -0500

    Exercise 1 complement
    Modified names of the project and files and also added the comment of the exercise statement.

--- c++ training/PointersTraining/PointerExercises/Project1/Source.cpp
+++ c++ training/PointersTraining/PointerExercises/Project1/Source.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+/*	Exercise 1 - Understanding Memory Addresses
+Create a program that will ask the user to enter an integer and assign the given value to a
+variable.Then, print the value to the console, and print the memory address of that variable.
+Then, ask for another valueand assign that value to the same variable.Print the valueand the
+memory address to the console.
+Finally, ask for one more value.This time, assign the value to a different variable.Print the
+valueand the memory address to the console.Do the results make sense to you ?*/
+
 int main()
 {
 	std::cout << "Please enter a number: ";
