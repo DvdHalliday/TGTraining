84d4ccfc8be2813c6e071bae1125eb8017a924b8
David <109089386+DvdHalliday@users.noreply.github.com>
2022-09-06 14:40:58 -0500

    Part Five
    Added every method const & no-const

--- c++ training/ArraysVectors/Arrays-Vectors/ArraysVectors/StaticArray.h
+++ c++ training/ArraysVectors/Arrays-Vectors/ArraysVectors/StaticArray.h
@@ -11,4 +11,44 @@ public:
 	{
 		return N;
 	}
+
+	T& operator[](int Index)
+	{
+		return Data[Index];
+	}
+
+	const T& operator[](int Index) const
+	{
+		return Data[Index];
+	}
+
+	T& Front()
+	{
+		return Data[0];
+	}
+	
+	const T& Front() const
+	{
+		return Data[0];
+	}
+
+	T& Back()
+	{
+		return Data[N-1];
+	}
+
+	const T& Back() const
+	{
+		return Data[N-1];
+	}
+
+	T* GetData()
+	{
+		return &Data[0];
+	}
+
+	const T* GetData() const
+	{
+		return &Data[0];
+	}
 };
